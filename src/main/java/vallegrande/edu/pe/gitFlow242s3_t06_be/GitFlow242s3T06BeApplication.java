0dd967465adc4cbcb8b356c030ba9f7404a262d7
cristhianYaranga <cristhian.yaranga@vallegrande.edu.pe>
2026-05-23 15:58:27 -0500

    feat(backend): implement endpoint to create tasks

---
+++ src/main/java/vallegrande/edu/pe/gitFlow242s3_t06_be/GitFlow242s3T06BeApplication.java
@@ -0,0 +1,13 @@
+package vallegrande.edu.pe.gitFlow242s3_t06_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlow242s3T06BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFlow242s3T06BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vallegrande/edu/pe/gitFlow242s3_t06_be/GitFlow242s3T06BeApplicationTests.java
@@ -0,0 +1,13 @@
+package vallegrande.edu.pe.gitFlow242s3_t06_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlow242s3T06BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
